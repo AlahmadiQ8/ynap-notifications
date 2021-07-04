4a0b13b905519fc722e5a8631b47e1cc6bb2ef22
Mohammad Mohammad <mmohammadq8@outlook.com>
2021-07-04 17:35:57 -0400

    remove redundant error check

--- YNABTransactionsNotifier/YNABService.cs
+++ YNABTransactionsNotifier/YNABService.cs
@@ -46,10 +46,6 @@ namespace YnabTransactionsNotifier
         {
             var endpoint = $"/v1/budgets/{BudgetId}/transactions/import";
             var result = await _httpClient.PostAsync(endpoint, new StringContent(string.Empty));
-            if (!result.IsSuccessStatusCode)
-            {
-                _logger.LogError($"{result.RequestMessage?.RequestUri?.OriginalString} failed with status code {result.StatusCode}");
-            }
 
             switch (result.StatusCode)
             {
